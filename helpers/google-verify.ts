835939159d3c357123f4895b6651cc42cc18c8e2
PerairaCC <carloscoto0103@gmail.com>
2021-10-08 16:56:20 -0600

    google-verify.ts: fuction verify added

---
+++ helpers/google-verify.ts
@@ -0,0 +1,20 @@
+import {OAuth2Client, TokenPayload} from 'google-auth-library';
+
+const client  = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+
+export async function verify( token : string  = '') {
+  const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: process.env.GOOGLE_CLIENT_ID,
+  });
+  const { name, picture, email} = ticket.getPayload() as TokenPayload;
+
+  return {
+      name,
+      picture,
+      email
+  }
+  
+
+}
