2f8e40ae93af161d584de8d69f8a17c33606689a
PerairaCC <carloscoto0103@gmail.com>
2021-10-09 22:52:48 -0600

    agents.test.ts: get an agent testing added

--- tests/agents.test.ts
+++ tests/agents.test.ts
@@ -14,3 +14,32 @@ mocha.describe('GET /api/agents', () => {
     });
 });
 
+mocha.describe('GET:id /api/agents/:id', () => {
+
+    // TODO: No valid token, No token, identification is not numeric, 
+
+    mocha.it('respond with json containing a single agent', done => {
+        request(API)
+            .get('/api/agents/004')
+            .set('Accept', 'application/json')
+            .set('x-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiIxMDIiLCJpYXQiOjE2MzM4Mzc4MzIsImV4cCI6MTYzMzg1MjIzMn0.JduoFYLmgbwDQccBzKGfxrjDXNyVmXGzwHvghdsllWs')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200, done);
+    });
+
+    mocha.it('respond with json "msg: Agent with that ID not found in the database." when user does not exists', done => {
+        request(API)
+            .get('/api/agents/058')
+            .set('Accept', 'application/json')
+            .set('x-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiIxMDIiLCJpYXQiOjE2MzM4Mzc4MzIsImV4cCI6MTYzMzg1MjIzMn0.JduoFYLmgbwDQccBzKGfxrjDXNyVmXGzwHvghdsllWs')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(404)
+            .expect({
+                msg : 'Agent with that ID not found in the database.'
+            })
+            .end( err => {
+                if(err) return done(err);
+                done();
+            });
+    });
+});
